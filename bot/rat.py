cc9f7cde5631f0fab59ca80be3f8862bffed6add
Yunitrish <yunthomas006@gmail.com>
2023-01-04 23:58:35 +0800

    rollback

--- bot/rat.py
+++ bot/rat.py
@@ -7,7 +7,7 @@ from Log import log
 import components
 from components import available_servers,pickup,jobs,departments,grades,get_ids
 NCUE = 764126539041734679
-token="MTAyMDM1OTg0NTU5NTA1NDA4MA.G_xkZr._TOqJLn7iTsN3FF9Y1AuGLg1uIBSArwLAKkSrM"
+token="MTAyMDM1OTg0NTU5NTA1NDA4MA.GLPdny.5NKYNxTBGhEB85cs6eaEP6ML4twQp4pFCdwfq8"
 guild_list:list[discord.Object] = []
 
 
@@ -59,8 +59,6 @@ class bot_client(discord.Client):
 client = bot_client()
 tree = app_commands.CommandTree(client)
 
-
-
 class department_select(Select):
     def __init__(self):
         super().__init__(
@@ -106,6 +104,51 @@ class select_view(View):
         super().__init__(timeout=timeout)
         self.add_item(item)
 
+class job_select(Select):
+    def __init__(self):
+        super().__init__(
+            placeholder ="select your job",
+            min_values=1,
+            max_values=1,
+            options=jobs
+        )
+    async def callback(self, interaction: discord.Interaction):
+        user = interaction.user
+        guild = interaction.guild
+        ########################################################################
+        def add_userdata(add):
+            out = []
+            lst = [line.strip('\n').split(" ") for line in open('database/jobs.txt').readlines()]
+            for i in lst:
+                cnt = 0
+                #把檔案重複名字更新只留最後一個
+                for j in out:
+                    if i[0]==j[0]:
+                        j[1]=i[1]
+                    else:
+                        cnt+=1
+                if cnt == len(out):
+                    out.append(i)
+            #如果新加入的user名字存在在檔案中 -> 更新檔案
+            cnt = 0
+            for i in out:
+                if add[0]==i[0]:
+                    i[1]=add[1]
+                else:
+                    cnt += 1
+            if cnt == len(out):
+                out.append(add)
+            with open('database/jobs.txt', 'w') as f:
+                for i in out:
+                    f.write(str(i[0])+" "+str(i[1])+"\n")
+        ########################################################################
+        add_userdata([str(user.name),str(self.values[0])])
+        await interaction.response.send_message(f"{user.name} select {self.values[0]} as job",ephemeral=True)
+        for i in self.options:
+            current =  get(guild.roles, name=i.emoji.name+"."+i.label)
+            if current.name.split(".")[1] == self.values[0]: await user.add_roles(current)
+            else:  await user.remove_roles(current)
+
 @tree.command(name="查詢機器人服務伺服器",description="查詢機器所在的服務器",guilds=guild_obj_list)
 async def self(i:discord.Interaction):
     history.println("<"+i.user.name+"> send a command "+self.name)
@@ -134,9 +177,9 @@ async def self(interaction:discord.Interaction):
             work_file.write(f"work-{user.name}-100\n")
     await interaction.response.send_message(f"work,{user.name},100\n",ephemeral=True)
 
-@tree.command(name="選工作",description="test of selector",guilds=guild_obj_list)
+@tree.command(name="選工作",description="選擇你的職業",guilds=guild_obj_list)
 async def self(interaction:discord.Interaction):
-    await interaction.response.send_message(f"select your job",view=select_view(components.job_select()),ephemeral=True)
+    await interaction.response.send_message(f"select your job",view=select_view(job_select()),ephemeral=True)
 
 @tree.command(name="選擇科系",description="選擇自己科系以加入相關討論區",guilds=guild_obj_list)
 async def self(interaction:discord.Interaction):
